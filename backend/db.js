c6c416a04dd92807a2f937a44c43da500fd76dbd
Kanch-prog <121807277+Kanch-prog@users.noreply.github.com>
2023-10-19 05:11:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'GoodLuck123!',
+  password: '   ',
   database: 'ecommerce_db'
 });
 
